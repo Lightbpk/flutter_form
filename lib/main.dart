d46063dd31d954005cbb389259ef87537a3f894f
Lightbpk <bpk735@gmail.com>
2021-03-01 18:43:47 +0700

    'emailadd'

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,48 @@ class MyFormState extends State {
           child: new Column(
             children: <Widget>[
               new Text(
-                'Введите текст',
+                'Введите рыло',
                 style: TextStyle(fontSize: 20.0),
               ),
               new TextFormField(validator: (value) {
-                if (value.isEmpty) return "Введите чтото";
+                if (value.isEmpty)
+                  return "Введите что-то";
+                else
+                  return null;
               }),
-              new SizedBox(height: 20.0,),
-              new RaisedButton(onPressed: () {
-                if (_formkey.currentState.validate())
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                    content: Text('успех форма заполнена'),
-                    backgroundColor: Colors.green,));
-              },
+              new SizedBox(
+                height: 20.0,
+              ),
+              new Text(
+                'Введите мыло',
+                style: TextStyle(fontSize: 20.0),
+              ),
+              new TextFormField(
+                validator: (value) {
+                  if (value.isEmpty) return "мыло то введите";
+                  String p =
+                      "[a-zA-Z0-9+.\_\%-+]{1,256}@[a-zA-Z0-9]{0,64}(.[a-zA-Z0-9]{0,25})+";
+                  RegExp regExp = new RegExp(p);
+                  if (regExp.hasMatch(value)) return null;
+                  return "че то вы мне парите";
+                },
+              ),
+              new SizedBox(
+                height: 20.0,
+              ),
+              new RaisedButton(
+                onPressed: () {
+                  if (_formkey.currentState.validate())
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('успех форма заполнена'),
+                      backgroundColor: Colors.green,
+                    ));
+                },
                 child: Text('проверить'),
                 color: Colors.blue,
-                textColor: Colors.white,),
+                textColor: Colors.white,
+              ),
             ],
-
           ),
         ));
   }
@@ -44,9 +68,9 @@ class MyFormState extends State {
 void main() {
   runApp(new MaterialApp(
       home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Форма ввода'),
-        ),
-        body: new MyForm(),
-      )));
+    appBar: new AppBar(
+      title: new Text('Форма ввода'),
+    ),
+    body: new MyForm(),
+  )));
 }
